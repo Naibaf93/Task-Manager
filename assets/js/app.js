de494f7df53a4d0cd15fc51e178e997ad6580856
Fabián Huertas <miloh9318@hotmail.com>
2025-06-20 21:09:34 -0500

    creando un evento submit

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,32 @@ const taskForm = document.getElementById("task-form");
 
 //Seleccionar el TaskList (Elemento Padre) donde se irán agregando todas las tareas
 const taskList = document.getElementById("task-list");
+
+//Llamado de la función que carga las tareas que ya estaban en el LOCALSTORAGE
+
+
+//Agregar el evento de SUBMIT
+taskForm.addEventListener("submit", (e) => {
+
+    //Prevenir el comportamiento por DEFAULT de refrescar la página en cada click al botón
+    e.preventDefault();
+
+    //Seleccionar el INPUT dentro del formulario
+    const taskInput = document.getElementById("task-input");
+
+    //Agarra el valor introducido en teclado por el usuario
+    const task = taskInput.value;
+    //Muestra dicho valor para confirmar que el código funciona hasta acá en la consola
+    console.log(task);
+
+    //Valida si es verdadero para agregar una nueva tarea
+      if(task){
+        //Agrega con APPEND la nueva tarea después de la tarea por default
+        //Para esto se llama a la función createTaskElement
+        taskList.append(createTaskElement(task));
+        //Guarda la tarea en el LOCAL STORAGE
+        storeTaskInLocalStorage(task);
+        //Limpia el valor del input
+        taskInput.value = "";
+    }    
+});
