f51837617afa8c56505a6b6ed6ae90246fe3c12c
Fabián Huertas <miloh9318@hotmail.com>
2025-06-20 20:22:48 -0500

    creando las priemras dos variables

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+//OBTENER LA INFORMACIÓN DEL FORMULARIO
+
+//Seleccionar el Formulario (Elemento Padre)
+const taskForm = document.getElementById("task-form");
+
+//Seleccionar el TaskList (Elemento Padre) donde se irán agregando todas las tareas
+const taskList = document.getElementById("task-list");
